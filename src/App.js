40bcef6bbee5a8de3927478bfad060b76108b01b
YanaKukhta <jack.smith.1999@mail.ru>
2021-01-03 20:09:10 +0300

    progress

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import React from 'react';
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 import Navigation from "./components/Navigation/Navigation";
+import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import { BrowserRouter, Route} from "react-router-dom";
 import './App.css';
 
 const App = () => {
  return (
-<div>
- <Header />
- <div class="_container main">
- <Profile />
- <Navigation />
- </div>
-</div>
+  <BrowserRouter>
+   <div>
+    <Header />
+    <div class="_container main">
+     <Route path='/messages' component={Dialogs} />
+     <Route path='/profile' component={Profile} />
+     <Route path='/news' component={News} />
+     <Route path='/music' component={Music} />
+     <Route path='/settings' component={Settings} />
+     <Navigation />
+    </div>
+   </div>
+  </BrowserRouter>
  );
 }
 
